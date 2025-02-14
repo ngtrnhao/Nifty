b3a202815d0d2b6abdbc5646dd586fbbbb738524
Nguyen Truong Nhat Hao <nguyentruongnhathao1922@gmail.com>
2025-02-14 22:23:50 +0700

    feat: Implement Footer component

--- frontend/src/components/layout/MainLayout.jsx
+++ frontend/src/components/layout/MainLayout.jsx
@@ -1,5 +1,5 @@
 import Navbar from './Navbar';
-
+import Footer from './Footer';
 const MainLayout = ({ children }) => {
   return (
     <div className="min-h-screen bg-gray-50">
@@ -7,6 +7,7 @@ const MainLayout = ({ children }) => {
       <main className="max-w-7x1 mx-auto px-4 py-8 sm:px-6 lg:px-8">
         {children}
       </main>
+      <Footer />
     </div>
   );
 };
